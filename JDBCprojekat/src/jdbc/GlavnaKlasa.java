54b9a8c34878eca71b48e9ab19ddf04b1090534f
Korisnik <Korisnik@192.168.194.250>
2020-04-27 19:12:05 +0200

    statement

--- JDBCprojekat/src/jdbc/GlavnaKlasa.java
+++ JDBCprojekat/src/jdbc/GlavnaKlasa.java
@@ -2,6 +2,7 @@ package jdbc;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import metode.JdbcMetode;
 
@@ -9,10 +10,21 @@ public class GlavnaKlasa {
 
 	public static void main(String[] args) {
 		
+		String drzava= "Srbija";
+		String grad = "Novi Sad";
+		String ulica= "Jove Zmaja 10";
+		
 		Connection konekcija = null;
 		try {
 			konekcija = JdbcMetode.uspostaviKonekciju();
 			System.out.println("Konekcija uspostavljena!!!");
+			//insert statement
+			String query = "INSERT INTO adrese VALUES(null,'"+ drzava +
+													"','"+ grad +
+													"','"+ ulica +"')";
+			Statement statement = konekcija.createStatement();
+			statement.execute(query);
+			System.out.println("Uspesno ste uneli podatak u bazu!");
 		} catch (SQLException e) {
 			System.out.println("Nema konekcije!!!");
 			e.printStackTrace();
